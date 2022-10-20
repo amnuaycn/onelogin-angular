b0e4d3b6132033724c64167270bf940d6a28b8ed
amnuaycn <115617221+amnuaycn@users.noreply.github.com>
2022-10-20 22:36:05 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 export const environment = {
   production: false,
   oidc : {
-    stsAuthority: 'https://egatdev-dev.onelogin.com/oidc/2/',
-    clientId: '1ff4f2e0-2c43-013b-5717-0ae5611e59df217225',
-    ClientSecret: '4423deeaa253253d8e5beb7c0e5ddb1da78bb41d156dbebe38931feed3889155',
+    stsAuthority: 'https://<onelogin subdomain>.onelogin.com/oidc/2/',
+    clientId: '<client_id>',
+    ClientSecret: '<client_secret>',
     clientRoot: 'http://localhost:4200/',
     clientScope: 'openid email profile'
   },
   api : {
-    clientId: '997fbc2543b27e1350aa77bd6c4ae750494c088ddfc42b57d52360b833507d8a',
-    ClientSecret: 'bc3fe5f02ccc74361cddcf11bcd6244ecb44abdc37e8ae05ef05d586dcbfa384',
-    apiRoot: 'https://egatdev-dev.onelogin.com'
+    clientId: '<api_client_id>',
+    ClientSecret: '<api_client_secret>',
+    apiRoot: 'https://<onelogin subdomain>.onelogin.com'
   }
 };
 
